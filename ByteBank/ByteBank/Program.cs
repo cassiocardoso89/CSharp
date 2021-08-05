72701cf1eb8c39691647853094ea9abb8aeb635b
Cássio Cardoso <cassiocardoso89@gmail.com>
2021-08-05 20:58:04 -0300

    03-ArgumentException ParamName e nameof

--- ByteBank/ByteBank/Program.cs
+++ ByteBank/ByteBank/Program.cs
@@ -10,45 +10,11 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
-            Metodo_1();
 
-            //ContaCorrente conta = new ContaCorrente(777, 999999);
-            //Console.WriteLine(ContaCorrente.TaxaOperacao);
+            ContaCorrente conta = new ContaCorrente(777, 999999);
+            Console.WriteLine(ContaCorrente.TaxaOperacao);
 
             Console.ReadLine();
-        }
-
-        private static void Metodo_1()
-        {
-            Console.WriteLine("M1: Início");
-
-            try
-            {
-                Metodo_2();
-            }
-            catch (DivideByZeroException)
-            {
-                Console.WriteLine("catch(DivideByZeroException)");
-            }
-            Console.WriteLine("M1: Fim");
-        }
-
-        private static void Metodo_2()
-        {
-            Console.WriteLine("M2: Início");
-            Metodo_3();
-            Console.WriteLine("M2: Fim");
-        }
-
-        private static void Metodo_3()
-        {
-            Console.WriteLine("M3: Início");
-
-            int numero = 10;
-            int divisor = 0;
-            int resultado = numero / divisor;
-
-            Console.WriteLine("M3: Fim");
         }
     }
 }
